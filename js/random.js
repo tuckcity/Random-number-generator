576a2562392e60876780402813e30d7a15d1b5fc
Alex Tucker <tucker.ale@gmail.com>
2023-07-16 12:21:45 +0700

    added comments

--- js/random.js
+++ js/random.js
@@ -3,25 +3,21 @@ const lowString = prompt(`Enter a low number!`);
 const highString = prompt(`Enter a high number!`);
 
 // Convert the input to a number
-
 const lowNumber = parseInt(lowString);
 const highNumber = parseInt(highString);
 
+// Create the range to apply to the random generator
 const rangeNumber = highNumber - lowNumber + 1;
-console.log(rangeNumber);
 
-// const randomNumber = Math.floor(Math.random() * (rangeNumber) + lowNumber);
-
-// console.log(`${randomNumber} is a number between ${lowNumber} and ${highNumber}!`);
-
+// Check if numbers were entered into the prompt
 if (rangeNumber) {
     const randomNumber = Math.floor(Math.random() * (rangeNumber) + lowNumber);
 
 // Create a message displaying the random number
-
 console.log(`${randomNumber} is a number between ${lowNumber} and ${highNumber}!`)
-} else {
-    alert(`You need to provide two numbers, please try again.`);
-}
 
+// If error, prompt user to enter a number
+} else {
+    alert(`You need to provide two numbers, please refresh the page and try again.`);
+}
 
